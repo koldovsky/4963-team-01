9be572121347ce695f9636ae723e60feb527c833
airdray <belous50409@gmail.com>
2026-03-25 14:03:42 +0200

    add Loader

--- js/index.js
+++ js/index.js
@@ -11,3 +11,12 @@ document.body.addEventListener("htmx:afterOnLoad", () => {
   loadedPartialsCount++;
   if (loadedPartialsCount === totalPartials) init();
 });
+
+window.onload = function() {
+    setTimeout(() => {
+      document.getElementById("loader").style.opacity = "0";
+      setTimeout(() => {
+        document.getElementById("loader").style.display = "none";
+      }, 1000);
+    }, 2500);
+};
