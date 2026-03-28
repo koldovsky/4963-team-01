2e56979a0c7e49917e717b84f8a199540039d962
Andrii Oliinyk <43649005+oland1979@users.noreply.github.com>
2026-03-28 18:22:23 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ function init() {
   import("./index.age-modal.js");
   import("./index.newsletter.js");
   import("./global.footer.js");
+  import("./index.awards-reveal.js"); 
+
 }
 
 const totalPartials = document.querySelectorAll(
