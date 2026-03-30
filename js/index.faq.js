0ef64a8ed228f1e070f26cd1f23a95a66b8900a3
but-d <dimacpoint@gmail.com>
2026-03-30 12:05:36 +0000

    fix class names in faq

--- js/index.faq.js
+++ js/index.faq.js
@@ -2,15 +2,15 @@ const faqItemHeaders = document.querySelectorAll(".faq__question-title");
 
 faqItemHeaders.forEach(faqItemHeader => {
     faqItemHeader.addEventListener("click", event => {
-        const currentlyActiveFaqItemHeader = document.querySelector(".faq__question-title.active");
+        const currentlyActiveFaqItemHeader = document.querySelector(".faq__question-title--active");
         if (currentlyActiveFaqItemHeader && currentlyActiveFaqItemHeader !== faqItemHeader) {
-            currentlyActiveFaqItemHeader.classList.toggle("active");
+            currentlyActiveFaqItemHeader.classList.toggle("faq__question-title--active");
             currentlyActiveFaqItemHeader.nextElementSibling.style.maxHeight = 0;
         }
 
-        faqItemHeader.classList.toggle("active");
+        faqItemHeader.classList.toggle("faq__question-title--active");
         const faqItemBody = faqItemHeader. nextElementSibling;
-        if(faqItemHeader.classList.contains("active")) {
+        if(faqItemHeader.classList.contains("faq__question-title--active")) {
             faqItemBody.style.maxHeight = faqItemBody.scrollHeight + "px";
         }
         else {
