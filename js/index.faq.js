49e21b419b30ab042b0fce5adcc3ec14e4e088ea
Dmytro Novykov <dimacpoint@gmail.com>
2026-04-01 22:35:24 +0300

    changed index.faq.partial.css removed @media (max-width); updated index.faq.js

--- js/index.faq.js
+++ js/index.faq.js
@@ -1,15 +1,20 @@
-const faqItemHeaders = document.querySelectorAll(".faq__question-title");
+const faqQuestions = document.querySelector(".faq__questions");
 
-faqItemHeaders.forEach((faqItemHeader) => {
-  faqItemHeader.addEventListener("click", () => {
-    const currentlyActiveFaqItemHeader = document.querySelector(
+if (faqQuestions) {
+  faqQuestions.addEventListener("click", (event) => {
+    const faqItemHeader = event.target.closest(".faq__question-title");
+    console.log("Clicked FAQ item header:", faqItemHeader);
+    if (!faqItemHeader || !faqQuestions.contains(faqItemHeader)) return;
+
+    const currentlyActiveFaqItemHeader = faqQuestions.querySelector(
       ".faq__question-title--active",
     );
+
     if (
       currentlyActiveFaqItemHeader &&
       currentlyActiveFaqItemHeader !== faqItemHeader
     ) {
-      currentlyActiveFaqItemHeader.classList.toggle(
+      currentlyActiveFaqItemHeader.classList.remove(
         "faq__question-title--active",
       );
       currentlyActiveFaqItemHeader.nextElementSibling.style.maxHeight = 0;
@@ -17,10 +22,11 @@ faqItemHeaders.forEach((faqItemHeader) => {
 
     faqItemHeader.classList.toggle("faq__question-title--active");
     const faqItemBody = faqItemHeader.nextElementSibling;
+
     if (faqItemHeader.classList.contains("faq__question-title--active")) {
       faqItemBody.style.maxHeight = faqItemBody.scrollHeight + "px";
     } else {
       faqItemBody.style.maxHeight = 0;
     }
   });
-});
+}
